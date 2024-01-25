1fb0e9c88822b9eb405eac3220e7455c19f89dc9
errahul <errahul.rp@gmail.com>
2024-01-25 23:15:36 +0530

    Update Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -4,7 +4,7 @@ const Education = () => {
     return (
         <div>
             <div className=' bg-[#121212]' id='Education'>
-                <div className='-mt-10 mr-4 sm:mr-6 md:mr-10 lg:mr-20 ml-4 sm:ml-60 md:ml-64 lg:ml-96 py-4 sm:py-6 md:py-8 lg:py-10 text-white'>
+                <div className='-mt-1 mr-4 sm:mr-6 md:mr-10 lg:mr-20 ml-4 sm:ml-60 md:ml-64 lg:ml-96 py-4 sm:py-6 md:py-8 lg:py-10 text-white'>
                     <h1 className='font-Garamond text-xl'>Education</h1>
                     <div className=' mt-3 w-[80%] border border-b-0 border-white'></div>
                     <div className=' mt-10 font-Asul text-sm sm:text-base text-gray-300'>Shivaji Univercity, Kolhapur </div>
